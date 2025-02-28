0db931cb35c890587fef01d040f1f020571615ef
joshp523 <joshp523@byu.edu>
2025-02-27 23:40:33 -0700

    went on a wild goose chase back there but we're all right now.

--- src/dashboard/dashboard.jsx
+++ src/dashboard/dashboard.jsx
@@ -2,8 +2,48 @@ import React from 'react';
 
 export function Dashboard() {
     return (
-        <main className='container-fluid bg-secondary text-center'>
-            <div> Dashboard displayed here</div>
+        <main className='container-fluid'>
+            <div className="item">
+                <h2>Your Monthly Spending</h2>
+                <div id="picture">
+                    <img src="images/placeholder.jpg" alt="Pie chart" />
+                </div>
+            </div>
+            <div className="item">
+                <h2>Log a New Transaction</h2>
+                <form action="form.html" method="post" id="transactionForm">
+                    <select name="type">
+                        <option>Select an option</option>
+                        <option>Expense</option>
+                        <option>Income</option>
+                    </select>
+                    <select name="category">
+                        <option>Select an option</option>
+                        <option>Piano Lessons</option>
+                        <option>Stipend</option>
+                        <option>Reimbursement</option>
+                        <option>Tithing</option>
+                        <option>Car Repair</option>
+                        <option>School</option>
+                        <option>Groceries</option>
+                        <option>Gifts</option>
+                        <option>Gas</option>
+                        <option>Wholesome Recreational Activities</option>
+                        <option>Home</option>
+                        <option>clothes</option>
+                        <option>classifieds</option>
+                        <option>Rent</option>
+                        <option>Utilities</option>
+                        <option>savings</option>
+                    </select>
+                    <input type="text" name="amount" placeholder="$ amount" required></input>
+                    <button type="submit" className="button">Submit</button>
+                </form>
+            </div>
+
+            <div className="item">
+                <button onclick="location.href='transactions.html';" className="button2">Transaction History</button>
+            </div>
         </main>
     );
 }
--- src/login/login.jsx
+++ src/login/login.jsx
@@ -2,8 +2,27 @@ import React from 'react';
 
 export function Login() {
     return (
-        <main className='container-fluid bg-secondary text-center'>
-            <div> Login displayed here</div>
+        <main className='container-fluid'>
+            <div className="item">
+                <h2>Welcome to the Family Finance Manager</h2>
+                <form method="get" action="dashboard.html">
+                    <div>
+                        <span>Name</span>
+                        <input type="text" className="login-input" placeholder="your name" />
+                    </div>
+                    <div>
+                        <span>Password</span>
+                        <input type="password" className="login-input" placeholder="password" />
+                    </div>
+                    <div>
+                        <span>Family ID</span>
+                        <input type="password" className="login-input" placeholder="family ID" />
+                    </div>
+
+                    <button type="submit" className="button">Login</button>
+                    <button type="submit" className="button">Create an account</button>
+                </form>
+            </div>
         </main>
     );
 }
