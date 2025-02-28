37db24ccdbe29b4165ba7ef4f233938fc7d5575a
joshp523 <joshp523@byu.edu>
2025-02-27 21:37:43 -0700

    added some .jsx files

--- src/app.jsx
+++ src/app.jsx
@@ -3,4 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './app.css';
 
 export default function App() {
-    return <div className='body bg-dark text-light'>App will display here</div>;
+    return <div className='body bg-dark text-light'> 
+    <header>
+    <h1>Family Budget</h1>
+
+    <nav>
+      <menu class="nav-menu">
+        <li><a href="index.html">Logout</a></li>
+        <li><a href="dashboard.html">Dashboard</a></li>
+        <li><a href="transactions.html">Transaction History</a></li>
+      </menu>
+    </nav>
+
+    <hr />
+  </header>
+
+  <main className='container-fluid bg-secondary text-center'>App components go here</main>
+  
+  <footer>
+    <hr />
+    <span className="footer-text">a startup by Joshua Riley</span>
+    <br />
+    <a href="https://github.com/Joshp523/startup.git" className="github-link">GitHub</a>
+  </footer>
+  
+  </div>;
+}
