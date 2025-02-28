fd3111e176e1732108b9ac73b1c0e58176eb7b3c
joshp523 <joshp523@byu.edu>
2025-02-27 21:37:43 -0700

    added some .jsx files

---
+++ src/transactions/transactions.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export function Transactions() {
+    return (
+        <main className='container-fluid bg-secondary text-center'>
+            <div> Transactions displayed here</div>
+        </main>
+    );
+}
