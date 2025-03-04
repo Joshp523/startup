4aba4df12fc14861a9cb922300e6037bedda8ad7
joshp523 <joshp523@byu.edu>
2025-03-04 14:04:47 -0700

    added a return button on the transaction page

--- src/transactions/transactions.jsx
+++ src/transactions/transactions.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 export function Transactions() {
+
+    const navigate = useNavigate();
+
     return (
         <main className='container-fluid text-center'>
             <div className="item">
@@ -13,17 +17,20 @@ export function Transactions() {
                             <span>Family Member</span>
                         </div>
                         <div className="transaction-row">
-                            <span style={{color: 'green'}} className="amount">+60</span>
+                            <span style={{ color: 'green' }} className="amount">+60</span>
                             <span className="category">Piano Lessons</span>
                             <span className="member">Meishe</span>
                         </div>
                         <div className="transaction-row">
-                            <span style={{color: 'red'}} className="amount">-105</span>
+                            <span style={{ color: 'red' }} className="amount">-105</span>
                             <span className="category">Car Parts</span>
                             <span className="member">Josh</span>
                         </div>
                     </div>
                 </section>
+            </div>
+            <div className="item">
+                <button className="button2" onClick={() => navigate('/dashboard')}>Back to Dashboard</button>
             </div>
         </main>
     );
