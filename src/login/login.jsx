885bdd459a1ba87bd786c85f8f72145b6d27cf60
joshp523 <joshp523@byu.edu>
2025-03-04 13:27:53 -0700

    implemented routing in login jsx

--- src/login/login.jsx
+++ src/login/login.jsx
@@ -1,34 +1,59 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-
 export function Login() {
+    
+    const [name, setName] = useState('');
+    const [password, setPassword] = useState('');
+    const [familyId, setFamilyId] = useState('');
+    const navigate = useNavigate();
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+        navigate('/dashboard');
+    };
+
+    const handleCreateAccount = (e) => {
+        e.preventDefault();
+        navigate('/dashboard');
+    };
+
     return (
         <main className='container-fluid'>
             <div className="item">
                 <h2>Welcome to the Family Finance Manager</h2>
-                <form method="get" action="Dashboard">
+                <form onSubmit= {handleLogin}>
                     <div>
                         <span>Name</span>
                         <input type="text" 
                                className="login-input" 
-                               placeholder="your name" />
+                               placeholder="your name"
+                               value={name}
+                               onChange={(e) => setName(e.target.value)} />
                     </div>
                     <div>
                         <span>Password</span>
                         <input type="password" 
                                className="login-input" 
-                               placeholder="password" />
+                               placeholder="password" 
+                               value={password}
+                               onChange={(e) => setPassword(e.target.value)} />
                     </div>
                     <div>
                         <span>Family ID</span>
                         <input type="password" 
                                className="login-input" 
-                               placeholder="family ID" />
+                               placeholder="family ID" 
+                               value={password}
+                               onChange={(e) => setFamilyId(e.target.value)} />
                     </div>
 
-                    <button type="submit" className="button">Login</button>
-                    <button type="submit" className="button">Create an account</button>
+                    <button type="submit" className="button">
+                        Login
+                    </button>
+                    <button type="submit" className="button" onClick={handleCreateAccount}>
+                        Create an account
+                    </button>
                 </form>
             </div>
         </main>
