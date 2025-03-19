acf5123e1df27bb1978ed055e7e3eca75846667d
joshp523 <joshp523@byu.edu>
2025-03-19 15:53:03 -0600

    login functionality

--- src/login/unauthenticated.jsx
+++ src/login/unauthenticated.jsx
@@ -16,7 +16,7 @@ export function Unauthenticated(props) {
         localStorage.setItem('familyID', familyId);
         props.onLogin(familyID);
         props.onLogin(userName);
-        navigate('/dashboard');
+        navigate('/authenticated');
     };
 
     async function handleCreateAccount() {
@@ -24,7 +24,7 @@ export function Unauthenticated(props) {
         localStorage.setItem('familyID', familyId);
         props.onLogin(familyID)
         props.onLogin(userName);
-        navigate('/dashboard');
+        navigate('/authenticated');
     };
 
     return (
